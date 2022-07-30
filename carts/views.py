191ef6ba48a3b84d06c662616d79840172285424
Satyam <satyam@example.com>
2022-07-30 23:56:45 +0530

    implement data into cart page

--- carts/views.py
+++ carts/views.py
@@ -30,7 +30,6 @@ def add_cart(request, product_id):
             cart = cart,
         )
     cart_item.save()
-    return HttpResponse(cart_item.quantity)
     return redirect('cart')
 #yoh5mflpoor8jy6qdmjlws94xrdb15gq
 def cart(request, total=0, quantity=0, cart_items=None):
@@ -40,6 +39,8 @@ def cart(request, total=0, quantity=0, cart_items=None):
         for cart_item in cart_items:
             total += (cart_item.product.price * cart_item.quantity)
             quantity += cart_item.quantity
+        tax = (2 * total)/100
+        grand_total = total + tax
     except ObjectNotExist:
         pass #just ignore
 
@@ -47,5 +48,7 @@ def cart(request, total=0, quantity=0, cart_items=None):
         'total': total,
         'quantity': quantity,
         'cart_items': cart_items,
+        'tax' : tax,
+        'grand_total': grand_total,
     }
     return render(request,'store/cart.html', context)
