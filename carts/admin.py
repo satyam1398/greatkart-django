c5bce689356049d52853c2d1abf07a9018daa5bb
Satyam <satyam@example.com>
2022-08-06 23:38:49 +0530

    Starting Product Variations and Variation Mannger

--- carts/admin.py
+++ carts/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import CartItem, Cart
 # Register your models here.
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('cart_id','date_added')
 
-admin.site.register(CartItem)
-admin.site.register(Cart)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ('product','cart','quantity','is_active')
+
+
+admin.site.register(CartItem,CartItemAdmin)
+admin.site.register(Cart,CartAdmin)
