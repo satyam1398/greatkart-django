9ceddce3b0999e851b94832558d7c5b6b828bbcc
Satyam <satyam@example.com>
2022-08-22 00:24:12 +0530

    registration login

--- accounts/views.py
+++ accounts/views.py
@@ -1,16 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms import RegistrationForm
+from .models import Account
+from django.contrib  import messages,auth
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def register(request):
-    form = RegistrationForm()
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            email = form.cleaned_data['email']
+            phone_number = form.cleaned_data['phone_number']
+            password = form.cleaned_data['password']
+            # confirm_password = form.cleaned_data['confirm_password']
+            username = email.split('@')[0]
+            user = Account.objects.create_user(first_name=first_name, last_name=last_name, email=email, username=username, password=password)
+            user.phone_number = phone_number
+            user.save()
+            messages.success(request, 'Registration Successful.')
+            return redirect('register')
+    else:
+        form = RegistrationForm()
     context = {
         'form': form,
     }
     return render(request, 'accounts/register.html', context)
 
 def login(request):
+    if request.method == "POST":
+        email = request.POST['email']
+        password = request.POST['password']
+        print(email)
+        print(password)
+        user = auth.authenticate(email=email, password=password)
+        print(user)
+        if user is not None:
+            auth.login(request,user)
+            return redirect('home')
+        else:
+            messages.error(request,'Invaild Login Credentials')
+            return redirect('login')
     return render(request, 'accounts/login.html')
 
+@login_required(login_url = 'login')
 def logout(request):
-    return
+    auth.logout(request)
+    messages.success(request, "Your are logged out")
+    return redirect('login')
