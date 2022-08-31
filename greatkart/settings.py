728dd3b943d92ea3f9e622a3efa80a3b1d39d952
Satyam <satyam@example.com>
2022-08-31 23:38:38 +0530

    add account settings

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -137,3 +137,11 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+
+# SMTP configuration
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_HOST_USER = "satyam.sharma19981310@gmail.com"
+#EMAIL_HOST_USER = 'satyam.sharma19981310@gamil.com'
+EMAIL_HOST_PASSWORD = "lpkrhydnwyobfnwx"
+EMAIL_USE_TLS = True
